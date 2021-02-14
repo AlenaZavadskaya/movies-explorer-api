dc466d016920c273c90a711a30b574a1620244c4
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2021-02-14 11:09:39 +0300

    add request validation by celebrate

--- routes/movies.js
+++ routes/movies.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
+const { movieValidation, movieIdValidation } = require('../middlewares/serverDataValidator');
 
 router.get('/movies', getMovies);
-router.post('/movies', createMovie);
-router.delete('/movies/:movieId', deleteMovie);
+router.post('/movies', movieValidation, createMovie);
+router.delete('/movies/:movieId', movieIdValidation, deleteMovie);
 
 module.exports = router;
