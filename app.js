abe205d01da09519b40813f9b266f7b37e5faa74
Alena Zavadskaya <alena1yakusheva@gmail.com>
2021-04-28 14:02:24 +0300

    edit allow cors

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ mongoose.connect(BASE_URL, {
 });
 
 const allowedCors = [
-  'https://alena.movies.students.nomoredomains.monster',
-  'http://alena.movies.students.nomoredomains.monster',
-  'https://www.alena.movies.students.nomoredomains.monster',
-  'http://www.alena.movies.students.nomoredomains.monster',
+  'https://alenazavadskaya.site.by',
+  'http://alenazavadskaya.site.by',
+  'https://www.alenazavadskaya.site.by',
+  'http://www.alenazavadskaya.site.by',
   'https://localhost:3001',
   'http://localhost:3001',
 ];
